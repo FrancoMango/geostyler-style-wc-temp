308f722a26168c8b728f03c538653b5df8fbf564
Francis Lapointe <francis.lapointe-c@transports.gouv.qc.ca>
2026-06-04 12:36:54 -0400

    refactor(geostyler-web-component): encapsulate registration logic in a function

--- src/geostyler-web-component.ts
+++ src/geostyler-web-component.ts
@@ -2,6 +2,19 @@ import { GeostylerWebComponent } from './GeostylerWebComponent.js';
 
 const GEOSTYLER_WEB_COMPONENT_TAG = 'geostyler-style-wc';
 
-if (!customElements.get(GEOSTYLER_WEB_COMPONENT_TAG)) {
-  customElements.define(GEOSTYLER_WEB_COMPONENT_TAG, GeostylerWebComponent);
+export function registerGeostylerWebComponent(
+  registry: CustomElementRegistry | undefined = globalThis.customElements
+): boolean {
+  if (!registry) {
+    return false;
+  }
+
+  if (registry.get(GEOSTYLER_WEB_COMPONENT_TAG)) {
+    return false;
+  }
+
+  registry.define(GEOSTYLER_WEB_COMPONENT_TAG, GeostylerWebComponent);
+  return true;
 }
+
+registerGeostylerWebComponent();
